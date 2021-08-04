95bbef2fac72903ce911020ec54066a808a33f32
himadri2110 <18it.himadri.shah@gmail.com>
2021-08-04 16:54:32 +0530

    Feat: Added privacy notice

--- main.js
+++ main.js
@@ -1,36 +1,35 @@
-let checkBtn = document.querySelector("#check-btn");
 let inputDate = document.querySelector("#input-date");
 let inputLucky = document.querySelector("#input-lucky");
+let formDate = document.querySelector("#form");
 let resultDiv = document.querySelector("#result");
+let icon = document.querySelector(".fa");
+let privacyDiv = document.querySelector(".privacy");
 
-checkBtn.addEventListener("click", () => {
-  let sum = 0,
-    rem = 0;
+icon.addEventListener("click", () => {
+	privacyDiv.style.display = "none";
+})
 
-  if (inputDate.value === "") {
-    resultDiv.innerText = "Please enter your birthdate";
-    resultDiv.style.color = "#ff2c2c";
-    resultDiv.style.backgroundColor = 'initial';
-  } else if (inputLucky.value === "") {
-    resultDiv.innerText = "Please enter your lucky number";
-    resultDiv.style.color = "#ff2c2c";
-    resultDiv.style.backgroundColor = 'initial';
-  } else {
-    let date = inputDate.value.split('-').join('');
-    while (date !== 0) {
-      rem = date % 10;
-      sum += rem;
-      date = Math.floor(date / 10);
-    }
-    console.log(date, sum);
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	let sum = 0, rem = 0;
 
-    if (sum % inputLucky.value === 0) {
-      resultDiv.innerText = "Yay! Your birthdate is lucky. 😍";
-      resultDiv.style.backgroundColor = "#88fd97";
-    } else {
-      resultDiv.innerText = "OOPS! Your birthdate is unlucky. 😓";
-      resultDiv.style.backgroundColor = "#ff2c2c";
-      resultDiv.style.color = "#000000";
-    }
-  }
+	let date = inputDate.value.split('-').join('');
+	while (date !== 0) {
+		rem = date % 10;
+		sum += rem;
+		date = Math.floor(date / 10);
+	}
+
+	if (sum % inputLucky.value === 0) {
+		resultDiv.style.display = 'block';
+
+		resultDiv.innerText = "Yay! Your birthdate is lucky. 😍";
+		resultDiv.style.backgroundColor = "#88fd97";
+	} else {
+		resultDiv.style.display = 'block';
+
+		resultDiv.innerText = "OOPS! Your birthdate is unlucky. 😓";
+		resultDiv.style.backgroundColor = "#ff2c2c";
+		resultDiv.style.color = "#000000";
+	}
 });
